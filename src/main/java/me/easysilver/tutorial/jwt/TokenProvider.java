4a03e5f633f695c2503d2668aeb1d1083a6217a9
Timo <developer.timo@gmail.com>
2021-05-14 21:16:29 +0900

    오타 수정

--- src/main/java/me/easysilver/tutorial/jwt/TokenProvider.java
+++ src/main/java/me/easysilver/tutorial/jwt/TokenProvider.java
@@ -32,8 +32,8 @@ public class TokenProvider implements InitializingBean {
     private Key key;
 
     public TokenProvider(
-            @Value("{jwt.secret}") String secret,
-            @Value("{jwt.token-validity-in-seconds}") long tokenValidityInMilliseconds) {
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.token-validity-in-seconds}") long tokenValidityInMilliseconds) {
         this.secret = secret;
         this.tokenValidityInMilliseconds = tokenValidityInMilliseconds * 1000;
     }
